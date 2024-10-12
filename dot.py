c0eae7407280addc52604eb1eb7d88d88ae71ce1
harishm <harishm762003@gmail.com>
2024-10-12 08:32:56 +0530

    feat : Add 'init' command

---
+++ dot.py
@@ -0,0 +1,65 @@
+
+import sys
+import argparse
+import os
+from repository import Repository
+
+class Dot:
+
+    """
+        init  -- initialize a repository
+        idx   -- index files and folders
+        save  -- commit the changes to the dot history
+        log   -- display the logs of the repository 
+    """
+
+    def idx(self , args):
+        print("idx" , args)
+    
+    def init(self , args):
+
+        repo = Repository(os.getcwd())
+        repo.create()
+    
+    def save(self , args):
+        print("save" , args.filenames)
+        
+
+    def log(self , args):
+        print("log" , args)
+        
+
+    def run(self):
+
+        cli = argparse.ArgumentParser(description="Version control sytem")
+
+        sub_parser = cli.add_subparsers(title="Commands" , dest="command")
+
+        init = sub_parser.add_parser("init")
+
+        idx  = sub_parser.add_parser("idx")
+        idx.add_argument("filenames" , nargs="+")
+
+        save = sub_parser.add_parser("save")
+        save.add_argument("filenames" , nargs="+")
+
+        log  = sub_parser.add_parser("log")
+        log.add_argument("-l" , "--limit" , type=int)
+
+        cli.add_argument("-v" , "-version" , help="Display the current version of dot")
+        
+        cli_args = cli.parse_args()
+
+        match cli_args.command:
+            case "init":
+                self.init(cli_args)
+            case "idx":
+                self.idx(cli_args)
+            case "save":
+                self.save(cli_args)
+            case "log":
+                self.log(cli_args)
+            case _:
+                cli.print_help()
+
+Dot().run()
---
+++ repository.py
@@ -0,0 +1,43 @@
+import os
+import configparser
+import shutil
+
+class Repository:
+
+    def __init__(self, path):
+
+        self.work_tree = path
+        self.dot_dir = os.path.join(path, ".dot")
+
+    def create(self):
+
+        if not os.path.exists(self.dot_dir):
+            os.mkdir(self.dot_dir)
+            os.mkdir(os.path.join(self.dot_dir, "objects"))
+            os.mkdir(os.path.join(self.dot_dir, "refs"))
+            os.mkdir(os.path.join(self.dot_dir, "refs", "head"))
+            os.mkdir(os.path.join(self.dot_dir, "refs", "tags"))
+            os.mkdir(os.path.join(self.dot_dir, "hooks"))
+
+            with open(os.path.join(self.dot_dir, "HEAD"), "w") as HEAD_FILE:
+                HEAD_FILE.write("ref: refs/heads/main\n")
+
+            with open(os.path.join(self.dot_dir, "description"), "w") as DESC_FILE:
+                DESC_FILE.write(
+                    "Unnamed repository; edit this file 'description' to name the repository."
+                )
+
+            with open(os.path.join(self.dot_dir, "config.ini"), "w") as CONFIG_FILE:
+
+                config = configparser.ConfigParser()
+                config.add_section("core")
+                config.set("core", "\trepositoryformatversion", "0")
+                config.set("core", "\tfilemode", "false")
+                config.set("core", "\tbare", "false")
+                config.write(CONFIG_FILE)
+
+        else:
+            user_option = input("Do you want to reinitialize this repository").strip()
+            if user_option.startswith("y"):
+                shutil.rmtree(self.dot_dir)
+                self.create()
