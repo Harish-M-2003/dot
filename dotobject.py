b4d9c30e673818da9ce10e252665f5fd4df47a04
harishm <harishm762003@gmail.com>
2024-10-17 08:53:25 +0530

    [chore] : comment methods & included 'object_format' attr.

--- dotobject.py
+++ dotobject.py
@@ -4,33 +4,39 @@ import hashlib
 
 class DotObject:
 
-    def serialize(self , data):
+    def __init__(self , object_format):
+
+        self.object_format = object_format
+
+    def serialize(self , data): ...
+
+    def deserialize(self , hash_value): ...
         
-        return zlib.compress(data)
+    #     return zlib.compress(data)
 
-    def deserialize(self , path , type_):
-        if os.path.exists(path):
+    # def deserialize(self , path , type_):
+    #     if os.path.exists(path):
                 
-            with open(path , "rb") as object_file:
-                decompressed_fmt = zlib.decompress(object_file.read()).split(b"@")
-                if decompressed_fmt[0].decode("utf-8") == type_:
-                    print(decompressed_fmt[-1].decode("utf-8"))
-                else:
-                    print("dot : Corrupted HEAD pointer file error")
-        else:
-                print("dot : Bad HEAD pointer")
+    #         with open(path , "rb") as object_file:
+    #             decompressed_fmt = zlib.decompress(object_file.read()).split(b"@")
+    #             if decompressed_fmt[0].decode("utf-8") == type_:
+    #                 print(decompressed_fmt[-1].decode("utf-8"))
+    #             else:
+    #                 print("dot : Corrupted HEAD pointer file error")
+    #     else:
+    #             print("dot : Bad HEAD pointer")
 
-    def get_object(self , repo , type_ , hash):
+    # def get_object(self , repo , type_ , hash):
 
-        if os.path.exists(repo.dot_dir):
-            path = os.path.join(repo.dot_dir , "objects" , hash[:2] , hash[2:])
+    #     if os.path.exists(repo.dot_dir):
+    #         path = os.path.join(repo.dot_dir , "objects" , hash[:2] , hash[2:])
          
-            self.deserialize(path , type_)
-        else:
-            print("Not a dot repo")
+    #         self.deserialize(path , type_)
+    #     else:
+    #         print("Not a dot repo")
 
-    def get_hash(self , path):
+    # def get_hash(self , path):
 
-        with open(path, "rb") as object_file:
+    #     with open(path, "rb") as object_file:
 
-            return hashlib.sha1(self.serialize(object_file.read())).hexdigest()
+    #         return hashlib.sha1(self.serialize(object_file.read())).hexdigest()
